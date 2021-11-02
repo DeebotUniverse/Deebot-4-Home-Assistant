85e5322ca92e9c85f4f6af0be7308ef87f321071
Robert Resch <robert@resch.dev>
2021-11-02 13:48:36 +0100

    add volume number entity (#10)
    * add volume number entity
    
    * rename water_amount to water
    
    * back to water_amount

--- custom_components/deebot/number.py
+++ custom_components/deebot/number.py
@@ -1,15 +1,16 @@
-"""Select module."""
+"""Number module."""
 import logging
 from typing import Any, Dict, List, Optional
 
-from deebot_client.commands import SetWaterInfo
-from deebot_client.events import StatusEventDto, WaterAmount, WaterInfoEventDto
+from deebot_client.commands import SetVolume
+from deebot_client.events import StatusEventDto, VolumeEventDto
 from deebot_client.events.event_bus import EventListener
 from deebot_client.vacuum_bot import VacuumBot
-from homeassistant.components.select import SelectEntity, SelectEntityDescription
+from homeassistant.components.number import NumberEntity, NumberEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from numpy import array_split
 
 from .const import DOMAIN
 from .helpers import get_device_info
@@ -29,20 +30,23 @@ async def async_setup_entry(
 
     new_devices = []
     for vacbot in hub.vacuum_bots:
-        new_devices.append(WaterInfoSelect(vacbot))
+        new_devices.append(VolumeEntity(vacbot))
 
     if new_devices:
         async_add_entities(new_devices)
 
 
-class WaterInfoSelect(SelectEntity):  # type: ignore
-    """Water info select entity."""
+class VolumeEntity(NumberEntity):  # type: ignore
+    """Volume number entity."""
 
-    entity_description = SelectEntityDescription(
-        key="water_amount",
+    entity_description = NumberEntityDescription(
+        key="volume",
         entity_registry_enabled_default=False,
-        icon="mdi:water",
+        icon="mdi:volume-medium",
     )
+    _attr_min_value = 0
+    _attr_max_value = 10
+    _attr_step = 1.0
 
     def __init__(self, vacuum_bot: VacuumBot):
         """Initialize the Sensor."""
@@ -57,9 +61,7 @@ class WaterInfoSelect(SelectEntity):  # type: ignore
 
         self._attr_name = f"{name}_{self.entity_description.key}"
         self._attr_unique_id = f"{device_info.did}_{self.entity_description.key}"
-
-        self._attr_options = [amount.display_name for amount in WaterAmount]
-        self._attr_current_option: Optional[str] = None
+        self._attr_value: Optional[float] = None
 
     @property
     def device_info(self) -> Optional[Dict[str, Any]]:
@@ -72,19 +74,36 @@ class WaterInfoSelect(SelectEntity):  # type: ignore
 
         async def on_status(event: StatusEventDto) -> None:
             if not event.available:
-                self._attr_current_option = None
+                self._attr_icon = "mdi:volume-medium"
+                self._attr_value = None
                 self.async_write_ha_state()
 
-        async def on_water_info(event: WaterInfoEventDto) -> None:
-            self._attr_current_option = event.amount.display_name
+        async def on_volume(event: VolumeEventDto) -> None:
+            if event.maximum is not None:
+                self._attr_max_value = event.maximum
+            self._attr_value = event.volume
+            self._set_icon(self._attr_value)
             self.async_write_ha_state()
 
         listeners: List[EventListener] = [
-            self._vacuum_bot.events.subscribe(WaterInfoEventDto, on_water_info),
+            self._vacuum_bot.events.subscribe(VolumeEventDto, on_volume),
             self._vacuum_bot.events.subscribe(StatusEventDto, on_status),
         ]
         self.async_on_remove(lambda: unsubscribe_listeners(listeners))
 
-    async def async_select_option(self, option: str) -> None:
-        """Change the selected option."""
-        await self._vacuum_bot.execute_command(SetWaterInfo(option))
+    def _set_icon(self, value: Optional[float]) -> None:
+        arrays = array_split(  # type: ignore
+            range(self._attr_min_value + 1, self._attr_max_value + 1), 3
+        )
+        if value == self._attr_min_value:
+            self._attr_icon = "mdi:volume-off"
+        elif value in arrays[0]:
+            self._attr_icon = "mdi:volume-low"
+        elif value in arrays[1]:
+            self._attr_icon = "mdi:volume-medium"
+        elif value in arrays[2]:
+            self._attr_icon = "mdi:volume-high"
+
+    async def async_set_value(self, value: float) -> None:
+        """Set new value."""
+        await self._vacuum_bot.execute_command(SetVolume(int(value)))
