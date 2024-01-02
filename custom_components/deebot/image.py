562a9df89ad13a7757c26497ba9a4936de7444f2
Robert Resch <robert@resch.dev>
2024-01-02 21:15:45 +0100

    Deprecate deebot.refresh (#493)

--- custom_components/deebot/image.py
+++ custom_components/deebot/image.py
@@ -71,8 +71,6 @@ class DeebotMap(
         """Set up the event listeners now that hass is ready."""
         await super().async_added_to_hass()
 
-        self._device.map.enable()
-
         async def on_info(event: CachedMapInfoEvent) -> None:
             self._attr_extra_state_attributes["map_name"] = event.name
 
@@ -80,14 +78,19 @@ class DeebotMap(
             self._attr_image_last_updated = event.when
             self.async_write_ha_state()
 
-        subscriptions = [
-            self._device.events.subscribe(self._capability.chached_info.event, on_info),
-            self._device.events.subscribe(self._capability.changed.event, on_changed),
-        ]
+        self._subscribe(self._capability.chached_info.event, on_info)
+        self._subscribe(self._capability.changed.event, on_changed)
 
         def on_remove() -> None:
-            for unsubscribe in subscriptions:
-                unsubscribe()
             self._device.map.disable()
 
         self.async_on_remove(on_remove)
+        self._device.map.enable()
+
+    async def async_update(self) -> None:
+        """Update the entity.
+
+        Only used by the generic entity update service.
+        """
+        await super().async_update()
+        self._device.map.refresh()
